ca2da1d42e39be065141c957df2bb753e0d880ea
roma <mavlutovr@ya.ru>
2020-12-01 13:50:26 +0300

    feat: create the repository

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # adaptive-swapper
 Simple changing the posiiton of elements (tags) depends on the width of the window.
+
+## Demo
+
+https://webdeveloper.pro/adaptive-swapper-demo
+
+## Install
+
+```
+npm install adaptive-swapper --save
+```
+
+## How to use
+
+```javascript
+import adaptiveSwapper from 'adaptive-swapper';
+```
+
+```javascript
+adaptiveSwapper({
+
+    // The position of the elements for the width 960 or less of the window
+    960: {
+
+        // Header
+        '#js-mobile-header': [
+            '*', // Already existing any element in #js-mobile-header
+            '#js-header-cart',
+            '#js-callback-button'
+        ],
+
+        // Menu
+        '#js-mobile-menu': [
+            '#js-search-form'
+        ]
+    },
+
+	// The position of the elements for the width 480 or less of the window
+    480: {
+
+        // Header
+        '#js-mobile-header': [
+            '*',
+            '#js-header-cart'
+        ],
+
+        // Menu
+        '#js-mobile-menu': [
+            '#js-search-form',
+            
+            // Moved from #js-mobile-header
+            '#js-callback-button'
+        ]
+    }
+
+});
+```
+
+
+
---
+++ index.js
@@ -0,0 +1,93 @@
+export default structure => {
+
+    let elements = {};
+    let lastWidth;
+
+
+    const getElement = (selector) => {
+      if (!elements[selector]) {
+        elements[selector] = $(selector);
+      }
+      return elements[selector];
+    };
+
+
+    const rePos = () => {
+
+      let fixI = 0;
+
+      let windowWidth = window.innerWidth;
+
+      let structureWidth;
+      for (let w in structure) {
+
+        if (windowWidth <= w) {
+          if (!structureWidth) {
+            structureWidth = w;
+          }
+
+          else {
+            structureWidth = Math.min(w, structureWidth);
+          }
+        }
+      }
+
+
+      if (lastWidth === structureWidth) return false;
+
+
+      if (structureWidth) {
+
+        let structure = structure[structureWidth];
+
+        $.each(structure, (containerSelector, childIds) => {
+
+          let $container = getElement(containerSelector);
+
+          $.each(childIds, function (i, childSelector) {
+
+            i -= fixI;
+
+            let $currentElement = $container.children(':eq(' + i + ')');
+            if (!$currentElement.is(childSelector)) {
+
+              let $targetElement = getElement(childSelector);
+
+              if ($targetElement.length) {
+
+                saveOriginalPlace($targetElement);
+
+                if (i) {
+                  $container.children(':eq(' + (i - 1) + ')').after($targetElement);
+                }
+
+                else {
+                  $container.prepend($targetElement);
+                }
+              }
+
+              else {
+                fixI++;
+              }
+            }
+          });
+        });
+      }
+
+
+      // Default position
+      else {
+        $.each(elements, (i, $element) => {
+          $element.toOriginalPlace();
+        });
+      }
+
+
+      lastWidth = structureWidth;
+    };
+
+
+    rePos();
+    $(window).on('resize', rePos);
+  };
+
